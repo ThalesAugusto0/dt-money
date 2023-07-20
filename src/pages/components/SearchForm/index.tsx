f161abf224390cbecffb004d757073f80cd604a2
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-07-20 00:56:55 -0300

    Aplicado o react hook form e feito um componente controlado

--- src/pages/components/SearchForm/index.tsx
+++ src/pages/components/SearchForm/index.tsx
@@ -1,12 +1,39 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { SearchFormContainer } from "./styles";
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const searchFormSchema = z.object({
+  query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  });
 
-      <button type="submit">
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register("query")}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
